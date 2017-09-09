8a454ed77da15f80e00f11f1de80d4bd0fc5c29c
tracykaur <tracykaur@yahoo.com>
2017-09-09 21:40:50 +1000

    Added user resources

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def projects
   end
+
+  def resources
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get '/about', {to: 'pages#about', as: 'about'}
   get '/projects', {to: 'pages#projects', as: 'projects'}
+  get '/resources', {to: 'pages#resources', as: 'resources'}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
